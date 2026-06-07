b1b5aaa3b374e7665b2ef935d984a6009707e79a
ramjane88 <Rummy5674@gmail.com>
2026-06-07 15:54:24 +0530

    fix: truncated files and next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-  },
-};
-
-// Disable SWC entirely on ARM64
-process.env.NEXT_DISABLE_SWC = "1";
-
+const nextConfig = {};
 module.exports = nextConfig;
